1d4977f61c61774abb42650a55f9eeb96ed16faa
Vince <vpicone@gmail.com>
2017-08-02 04:06:06 +0000

    Add color styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+import createPalette from 'material-ui/styles/palette';
+import cyan from 'material-ui/colors/cyan';
+import green from 'material-ui/colors/green';
+import red from 'material-ui/colors/red';
 import AutoSuggest from './AutoSuggest';
 import AntigenCard from './AntigenCard';
 import logo from './logo.svg';
 import './App.css';
 import AntibodyForm from './AntibodyForm';
 import fire from './fire';
+import {Collapse} from 'react-collapse';
+import GithubCorner from './GithubCorner'
+
+const theme = createMuiTheme({
+  palette: createPalette({
+    primary: cyan, // Purple and green play nicely together.
+    accent: {
+      ...green,
+      A400: '#00e677',
+    },
+    error: red,
+  }),
+});
+
 
 class App extends Component {
   
@@ -35,18 +55,25 @@ class App extends Component {
   render() {
     const selectedAntigen = this.state.selectedAntigen;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Antibuddy</h2>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <GithubCorner />
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Antibuddy</h1>
+          </div>
+          <p className="App-intro">
+            A red blood cell antigen resource.
+          </p>
+          <AutoSuggest fireSuggestions={ this.state.antigens } onSuggestionSelected={ this.onSuggestionSelected } />
+          {selectedAntigen ? 
+            <Collapse isOpened={true}>
+              <AntigenCard selectedAntigen={selectedAntigen} />
+            </Collapse>
+            : ''}
+          {/*<AntibodyForm />*/}
         </div>
-        <p className="App-intro">
-          A red blood cell antigen resource.
-        </p>
-        <AutoSuggest fireSuggestions={ this.state.antigens } onSuggestionSelected={ this.onSuggestionSelected } />
-        {selectedAntigen ? <AntigenCard selectedAntigen={selectedAntigen} /> : ''}
-        {/*<AntibodyForm />*/}
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/GithubCorner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Corner from 'react-github-corner';
+
+const GithubCorner = () => {
+    return <Corner octoColor="white" bannerColor="#00b8d4" href="https://github.com/vpicone/antibuddy" />
+}
+
+export default GithubCorner;
