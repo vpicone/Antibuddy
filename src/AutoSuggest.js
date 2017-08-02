cef26bd81f87f22f005f01ebe879ffd9639c9328
Vince <vpicone@gmail.com>
2017-08-01 21:41:16 -0500

    add style

--- src/AutoSuggest.js
+++ src/AutoSuggest.js
@@ -1,33 +1,33 @@
 // @flow
 /* eslint-disable react/no-array-index-key */
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Autosuggest from 'react-autosuggest';
-import TextField from 'material-ui/TextField';
-import Paper from 'material-ui/Paper';
-import { MenuItem } from 'material-ui/Menu';
-import match from 'autosuggest-highlight/match';
-import parse from 'autosuggest-highlight/parse';
-import { withStyles, createStyleSheet } from 'material-ui/styles';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Autosuggest from "react-autosuggest";
+import TextField from "material-ui/TextField";
+import Paper from "material-ui/Paper";
+import { MenuItem } from "material-ui/Menu";
+import match from "autosuggest-highlight/match";
+import parse from "autosuggest-highlight/parse";
+import { withStyles, createStyleSheet } from "material-ui/styles";
 
 function renderInput(inputProps) {
   const { classes, home, value, ref, ...other } = inputProps;
-  
+
   return (
     <div>
-    <TextField
-      autoFocus={true}
-      className={classes.textField}
-      value={value}
-      inputRef={ref}
-      InputProps={{
-        classes: {
-          input: classes.input,
-        },
-        ...other,
-      }}
-    />
+      <TextField
+        autoFocus={true}
+        className={classes.textField}
+        value={value}
+        inputRef={ref}
+        InputProps={{
+          classes: {
+            input: classes.input
+          },
+          ...other
+        }}
+      />
     </div>
   );
 }
@@ -35,7 +35,7 @@ function renderInput(inputProps) {
 function renderSuggestion(suggestion, { query, isHighlighted }) {
   const matches = match(suggestion.label, query);
   const parts = parse(suggestion.label, matches);
-  
+
   return (
     <MenuItem selected={isHighlighted} component="div">
       <div>
@@ -67,8 +67,6 @@ function getSuggestionValue(suggestion) {
   return suggestion.label;
 }
 
-
-
 function getSuggestions(value, fireSuggestions) {
   const inputValue = value.trim().toLowerCase();
   const inputLength = inputValue.length;
@@ -78,7 +76,8 @@ function getSuggestions(value, fireSuggestions) {
     ? []
     : fireSuggestions.filter(suggestion => {
         const keep =
-          count < 5 && suggestion.label.toLowerCase().slice(0, inputLength) === inputValue;
+          count < 5 &&
+          suggestion.label.toLowerCase().slice(0, inputLength) === inputValue;
 
         if (keep) {
           count += 1;
@@ -88,58 +87,58 @@ function getSuggestions(value, fireSuggestions) {
       });
 }
 
-const styleSheet = createStyleSheet('IntegrationAutosuggest', theme => ({
+const styleSheet = createStyleSheet("IntegrationAutosuggest", theme => ({
   container: {
     flex: 1,
     height: theme.spacing.unit * 8,
     maxWidth: 800,
-    width: '60%',
-    margin: 'auto',
+    width: "60%",
+    margin: "auto"
   },
   suggestionsContainerOpen: {
-    position: 'absolute',
-    width: '60%',
-    margin: 'auto',
+    position: "absolute",
+    width: "60%",
+    margin: "auto",
     maxWidth: 800,
     marginTop: theme.spacing.unit,
     marginBottom: theme.spacing.unit * 3,
     left: 0,
-    right: 0,
+    right: 0
   },
   suggestion: {
-    display: 'block',
+    display: "block"
   },
   suggestionsList: {
-    margin: 'auto',
+    margin: "auto",
     padding: 0,
-    listStyleType: 'none',
+    listStyleType: "none"
   },
   textField: {
-    width: '100%',
-  },
+    width: "100%"
+  }
 }));
 
 class IntegrationAutosuggest extends Component {
   state = {
-    value: '',
-    suggestions: [],
+    value: "",
+    suggestions: []
   };
 
   handleSuggestionsFetchRequested = ({ value }) => {
     this.setState({
-      suggestions: getSuggestions(value, this.props.fireSuggestions),
+      suggestions: getSuggestions(value, this.props.fireSuggestions)
     });
   };
 
   handleSuggestionsClearRequested = () => {
     this.setState({
-      suggestions: [],
+      suggestions: []
     });
   };
 
   handleChange = (event, { newValue }) => {
     this.setState({
-      value: newValue,
+      value: newValue
     });
   };
 
@@ -152,7 +151,7 @@ class IntegrationAutosuggest extends Component {
           container: classes.container,
           suggestionsContainerOpen: classes.suggestionsContainerOpen,
           suggestionsList: classes.suggestionsList,
-          suggestion: classes.suggestion,
+          suggestion: classes.suggestion
         }}
         renderInputComponent={renderInput}
         suggestions={this.state.suggestions}
@@ -165,9 +164,9 @@ class IntegrationAutosuggest extends Component {
         inputProps={{
           autoFocus: true,
           classes,
-          placeholder: 'Enter an antigen/antibody',
+          placeholder: "Enter an antigen/antibody",
           value: this.state.value,
-          onChange: this.handleChange,
+          onChange: this.handleChange
         }}
       />
     );
@@ -175,7 +174,7 @@ class IntegrationAutosuggest extends Component {
 }
 
 IntegrationAutosuggest.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styleSheet)(IntegrationAutosuggest);
